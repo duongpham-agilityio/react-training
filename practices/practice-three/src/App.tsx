547222bc06f5a3810b9662268c80cb65597fade8
duong.pham <duong.pham@asnet.com.vn>
2023-10-02 17:42:14 +0700

    Config zustand & react query

--- practices/practice-three/src/App.tsx
+++ practices/practice-three/src/App.tsx
@@ -2,9 +2,16 @@ import { ChakraProvider } from '@chakra-ui/react';
 
 // Themes
 import { customTheme } from '@/themes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient: QueryClient = new QueryClient();
 
 const App = () => {
-  return <ChakraProvider theme={customTheme}></ChakraProvider>;
+  return (
+    <ChakraProvider theme={customTheme}>
+      <QueryClientProvider client={queryClient}></QueryClientProvider>
+    </ChakraProvider>
+  );
 };
 
 export default App;
