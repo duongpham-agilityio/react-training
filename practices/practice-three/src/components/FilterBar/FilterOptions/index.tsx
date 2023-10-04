87ac21de9c6c3eb4647b2cc5e4578d09be4911e8
duong.pham <phamtanduongtk29@gmail.com>
2023-10-05 00:46:58 +0700

    Update filter bar component

--- practices/practice-three/src/components/FilterBar/FilterOptions/index.tsx
+++ practices/practice-three/src/components/FilterBar/FilterOptions/index.tsx
@@ -13,7 +13,7 @@ export const FilterOptions = memo(() => (
         <ListItem key={value}>
           <Button
             as={Link} // Todo: Update to later
-            href={value} // Todo: Update to later
+            href={`?filter_option=${value}`} // Todo: Update to later
             border="1px solid"
             borderColor="blackAlpha.300"
             borderRadius={25}
