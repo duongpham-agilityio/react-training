72a6f1744085fecbb5cbfa23778e780cdb51368c
duong.pham <phamtanduongtk29@gmail.com>
2023-10-04 23:59:45 +0700

    Update UI MainLayout

--- practices/practice-three/src/components/Layout/index.tsx
+++ practices/practice-three/src/components/Layout/index.tsx
@@ -1,29 +1,25 @@
 import { Box, Container, Flex } from '@chakra-ui/react';
 import { ReactNode, memo } from 'react';
-import { FilterBar, ProductCard, SideBar } from '..';
-import { productCardProps } from '@/mocks';
+
+// Components
+import { SideBar } from '@/components';
 
 // Todo: Change children to <Outlet /> when I setup router
 export const MainLayout = memo(({ children }: { children?: ReactNode }) => {
   return (
-    <Container minH="100vh" bg="gray.100">
-      <Flex h="full">
-        <Box w={1141} py={32}>
+    <Container minH="100vh">
+      <Flex>
+        <Box w={1024} py={10}>
           {children}
-          <FilterBar value="" onChange={() => {}} />
-          {Array.from({ length: 30 }).map(() => (
-            <ProductCard {...productCardProps} />
-          ))}
         </Box>
-        <Flex
-          flex={1}
-          h="100vh"
-          justifyContent="flex-end"
-          alignItems="center"
-          // position="fixed"
+        <Box
+          position="fixed"
+          top="50%"
+          right="14%"
+          transform="translateY(-50%)"
         >
           <SideBar />
-        </Flex>
+        </Box>
       </Flex>
     </Container>
   );
