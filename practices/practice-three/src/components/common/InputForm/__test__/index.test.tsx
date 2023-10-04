a06a42edda83df0b76e588cb0f022d1549948cd2
duong.pham <duong.pham@asnet.com.vn>
2023-10-04 15:53:12 +0700

    Implement unit test for InputForm

---
+++ practices/practice-three/src/components/common/InputForm/__test__/index.test.tsx
@@ -0,0 +1,34 @@
+import { fireEvent, render } from '@testing-library/react';
+
+// Components
+import { InputForm } from '..';
+
+// Mocks
+import { inputFormProps } from '@/mocks';
+
+const setup = (props = inputFormProps) => render(<InputForm {...props} />);
+
+describe('InputForm', () => {
+  it('Match to snapshot', () => {
+    const { container } = setup();
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('Change value', () => {
+    const onChange = jest.fn();
+    const { getByPlaceholderText } = setup({
+      ...inputFormProps,
+      onChange,
+    });
+    const input = getByPlaceholderText(inputFormProps.placeholder ?? '');
+
+    fireEvent.change(input, {
+      target: {
+        value: 'a',
+      },
+    });
+
+    expect(onChange).toBeCalled();
+  });
+});
