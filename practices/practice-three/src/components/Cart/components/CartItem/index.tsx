5399a2640672928ad67a29a02265a1130cf6dfd1
duong.pham <phamtanduongtk29@gmail.com>
2023-10-06 00:21:56 +0700

    Create CartItem

--- practices/practice-three/src/components/Cart/components/CartItem/index.tsx
+++ practices/practice-three/src/components/Cart/components/CartItem/index.tsx
@@ -1,8 +1,20 @@
-import { memo } from 'react';
-import { HStack } from '@chakra-ui/react';
+import { memo, useCallback } from 'react';
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  IconButton,
+  Image,
+  Text,
+} from '@chakra-ui/react';
 
 // Types
 import { ICart } from '@/interface';
+
+// Icons
+import { Decrease, Increase, Trash } from '@/assets/icons';
+import isEqual from 'react-fast-compare';
 
 export interface CartItemProps {
   data: ICart;
@@ -10,4 +22,96 @@ export interface CartItemProps {
   onChangeQuantity: (productId: number, currentQuantity: number) => void;
 }
 
-export const CartItem = memo((): JSX.Element => <HStack>asdasdasd</HStack>);
+const areCompare = (
+  prevProps: CartItemProps,
+  nextProps: CartItemProps,
+): boolean => isEqual(prevProps.data, nextProps.data);
+
+export const CartItem = memo((props: CartItemProps): JSX.Element => {
+  const { data, onChangeQuantity, onRemove } = props;
+  const { name, description, imageURL, productId, price, quantity } = data;
+
+  const handleIncrease = useCallback((): void => {
+    onChangeQuantity(productId, quantity + 1);
+  }, [onChangeQuantity, productId, quantity]);
+
+  const handleDecrease = useCallback((): void => {
+    onChangeQuantity(productId, quantity - 1);
+  }, [onChangeQuantity, productId, quantity]);
+
+  const handleRemove = useCallback((): void => {
+    onRemove(productId);
+  }, [onRemove, productId]);
+
+  return (
+    <Flex
+      w="full"
+      border="1px solid"
+      borderColor="gray.300"
+      borderRadius={10}
+      py={2}
+      px={3}
+    >
+      <Flex flex={1} gap={18} alignItems="center">
+        <Image
+          src={imageURL}
+          objectFit="cover"
+          boxSize="80px"
+          borderRadius={5}
+        />
+
+        {/* Show info */}
+        <Box>
+          <Heading
+            fontSize={{
+              base: 14,
+              xl: 18,
+            }}
+          >
+            {name}
+          </Heading>
+          <Text
+            fontSize={{
+              base: 12,
+              xl: 14,
+            }}
+            fontWeight="regular"
+          >
+            {description}
+          </Text>
+        </Box>
+      </Flex>
+
+      {/* Actions */}
+      <Center flex={1} justifyContent="space-between">
+        {/* Quantity */}
+        <Center flex={1}>
+          <IconButton
+            aria-label="Button decrease quantity"
+            icon={<Decrease />}
+            onClick={handleDecrease}
+          />
+          <Text>{quantity}</Text>
+          <IconButton
+            aria-label="Button increase quantity"
+            icon={<Increase />}
+            onClick={handleIncrease}
+          />
+        </Center>
+
+        {/* Price */}
+        <Text flex={1} textAlign="center">
+          ${price}
+        </Text>
+
+        <Center flex={1}>
+          <IconButton
+            aria-label="Button remove from cart"
+            icon={<Trash />}
+            onClick={handleRemove}
+          />
+        </Center>
+      </Center>
+    </Flex>
+  );
+}, areCompare);
