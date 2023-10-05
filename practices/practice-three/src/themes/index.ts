9dd64f3782ef9f3a34985dc46e561ceeaa98d6f7
duong.pham <duong.pham@asnet.com.vn>
2023-10-05 12:26:25 +0700

    Remove custom breakpoint

--- practices/practice-three/src/themes/index.ts
+++ practices/practice-three/src/themes/index.ts
@@ -1,13 +1,7 @@
 import { extendTheme } from '@chakra-ui/react';
 
 // Base themes
-import {
-  colors,
-  fonts,
-  fontWeights,
-  shadows,
-  breakpoints,
-} from '@/themes/base';
+import { colors, fonts, fontWeights, shadows } from '@/themes/base';
 import { button, link, container } from '@/themes/components';
 
 export const customTheme = extendTheme({
@@ -15,7 +9,6 @@ export const customTheme = extendTheme({
   fonts,
   fontWeights,
   shadows,
-  breakpoints,
   components: {
     ...button,
     ...link,
