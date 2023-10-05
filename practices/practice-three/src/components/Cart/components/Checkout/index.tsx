7d9a9a617027c66320a84de3f078ae48488286d7
duong.pham <phamtanduongtk29@gmail.com>
2023-10-06 00:40:58 +0700

    Create checkout component

---
+++ practices/practice-three/src/components/Cart/components/Checkout/index.tsx
@@ -0,0 +1,46 @@
+import { Box, Button, Divider, Heading, Text } from '@chakra-ui/react';
+import { memo } from 'react';
+
+export interface CheckoutProps {
+  total: number;
+  onCheckout: () => void;
+}
+
+export const Checkout = memo(
+  ({ total, onCheckout }: CheckoutProps): JSX.Element => {
+    return (
+      <Box>
+        <Heading
+          textAlign="center"
+          fontSize={{
+            base: 16,
+            xl: 22,
+          }}
+          textTransform="uppercase"
+          padding={5}
+        >
+          Checkout
+        </Heading>
+
+        <Divider />
+
+        {/* Info */}
+        <Box py={5}>
+          <Text>Total: {`${total}`}</Text>
+          <Button
+            textTransform="uppercase"
+            bg="green.10"
+            w="full"
+            mt={5}
+            _hover={{
+              bg: 'green.20',
+            }}
+            onClick={onCheckout}
+          >
+            Checkout
+          </Button>
+        </Box>
+      </Box>
+    );
+  },
+);
