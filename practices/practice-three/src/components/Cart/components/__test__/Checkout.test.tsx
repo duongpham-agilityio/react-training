8fb89266b42b4b2f17982d81f6dff8c199796c28
duong.pham <phamtanduongtk29@gmail.com>
2023-10-06 00:41:31 +0700

    Implement unit test for checkout component

---
+++ practices/practice-three/src/components/Cart/components/__test__/Checkout.test.tsx
@@ -0,0 +1,28 @@
+import { fireEvent, render } from '@testing-library/react';
+
+// Components
+import { Checkout, CheckoutProps } from '..';
+
+const onCheckout = jest.fn();
+const init: CheckoutProps = {
+  total: 200,
+  onCheckout,
+};
+
+const setup = (props = init) => render(<Checkout {...props} />);
+
+describe('Checkout', () => {
+  it('Match to snapshot', () => {
+    const { container } = setup();
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('onCheckout', () => {
+    const { getByRole } = setup();
+
+    fireEvent.click(getByRole('button'));
+
+    expect(onCheckout).toMatchSnapshot();
+  });
+});
