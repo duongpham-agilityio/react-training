7d57b9066720ee5c81b04786d81aaba716376708
duong.pham <duong.pham@asnet.com.vn>
2023-10-05 12:34:55 +0700

    Responsive the medium viewport for Layout

--- practices/practice-three/src/components/Layout/index.tsx
+++ practices/practice-three/src/components/Layout/index.tsx
@@ -7,15 +7,27 @@ import { SideBar } from '@/components';
 // Todo: Change children to <Outlet /> when I setup router
 export const MainLayout = memo(({ children }: { children?: ReactNode }) => {
   return (
-    <Container minH="100vh" bg="gray.100">
-      <Flex>
-        <Box w={1024} py={10}>
+    <Container minH="100vh" py={51}>
+      <Flex justifyContent="space-between">
+        <Box flex={1} py={10}>
           {children}
         </Box>
+
         <Box
+          w={{
+            '2xl': 350,
+            md: 200,
+          }}
+          h="100vh"
+        ></Box>
+        <Box
+          w="fit-content"
           position="fixed"
           top="50%"
-          right="14%"
+          right={{
+            '2xl': 128,
+            md: 10,
+          }}
           transform="translateY(-50%)"
         >
           <SideBar />
