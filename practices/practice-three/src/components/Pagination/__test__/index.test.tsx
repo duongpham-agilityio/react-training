6f9392641d0ec4558a3b8496a744eed333729984
duong.pham <duong.pham@asnet.com.vn>
2023-10-05 13:36:48 +0700

    Create mock props

---
+++ practices/practice-three/src/components/Pagination/__test__/index.test.tsx
@@ -0,0 +1,56 @@
+import { fireEvent, render } from '@testing-library/react';
+
+// Components
+import { Pagination } from '..';
+
+// Mocks
+import { defaultPropsPagination } from '@/mocks';
+
+const setup = (props = defaultPropsPagination) =>
+  render(<Pagination {...props} />);
+
+describe('Pagination', () => {
+  it('Match to snapshot', () => {
+    const { container } = setup();
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('Re-render', () => {
+    const { rerender } = setup();
+
+    rerender(<Pagination {...defaultPropsPagination} data={[1, 2, 3]} />);
+  });
+
+  it('onClick next button', () => {
+    const onNextPage = jest.fn();
+    const { getByText } = setup({ ...defaultPropsPagination, onNextPage });
+
+    fireEvent.click(getByText('Next'));
+
+    expect(onNextPage).toBeCalled();
+  });
+
+  it('onClick prev button', () => {
+    const onPreviousPage = jest.fn();
+    const { getByText } = setup({
+      ...defaultPropsPagination,
+      currentPage: 2,
+      isPrevPage: true,
+      onPreviousPage,
+    });
+
+    fireEvent.click(getByText('Prev'));
+
+    expect(onPreviousPage).toBeCalled();
+  });
+
+  it('onClick next button', () => {
+    const onChangePage = jest.fn();
+    const { getByText } = setup({ ...defaultPropsPagination, onChangePage });
+
+    fireEvent.click(getByText('1'));
+
+    expect(onChangePage).toBeCalled();
+  });
+});
