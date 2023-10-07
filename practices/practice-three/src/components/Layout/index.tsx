1f7ec56294cacd2464e63f0b2e113d2b67b2891c
duong.pham <duong.pham@asnet.com.vn>
2023-10-07 14:09:32 +0700

    Create styles for the logo of the website

--- practices/practice-three/src/components/Layout/index.tsx
+++ practices/practice-three/src/components/Layout/index.tsx
@@ -1,35 +1,71 @@
-import { Box, Container, Flex } from '@chakra-ui/react';
+import {
+  Box,
+  Center,
+  Container,
+  Divider,
+  Flex,
+  Image,
+  Link,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
 import { ReactNode, memo } from 'react';
 
 // Components
 import { SideBar } from '@/components';
 
+// Images
+import Logo from '@/assets/images/logo.png';
+
 // Todo: Change children to <Outlet /> when I setup router
 export const MainLayout = memo(({ children }: { children?: ReactNode }) => (
-  <Container minH="100vh" py={51}>
-    <Flex justifyContent="space-between">
-      <Box flex={1}>{children}</Box>
-
-      <Box
-        w={{
-          base: 100,
-          md: 230,
-          '2xl': 280,
-        }}
-        h="calc(100vh - 110px)"
-      ></Box>
-      <Box
-        w="fit-content"
-        position="fixed"
-        top="50%"
-        right={{
-          base: 10,
-          '2xl': 128,
-        }}
-        transform="translateY(-50%)"
+  <Container>
+    <VStack>
+      <Center
+        as={Link}
+        py={2}
+        gap={3}
+        href="/"
+        w="full"
+        justifyContent="flex-start"
       >
-        <SideBar />
-      </Box>
-    </Flex>
+        <Image
+          w={45}
+          h={45}
+          src={Logo}
+          objectFit="cover"
+          borderRadius={5}
+          alt="Logo for website"
+        />
+        <Text fontSize="18" letterSpacing={2}>
+          Agility
+        </Text>
+      </Center>
+      <Divider />
+      <Flex w="full" justifyContent="space-between">
+        <Box flex={1}>{children}</Box>
+
+        <Box
+          w={{
+            base: 100,
+            md: 230,
+            '2xl': 280,
+          }}
+          minH="100vh"
+        ></Box>
+        <Box
+          w="fit-content"
+          position="fixed"
+          top="50%"
+          right={{
+            base: 10,
+            '2xl': 128,
+          }}
+          transform="translateY(-50%)"
+        >
+          <SideBar />
+        </Box>
+      </Flex>
+    </VStack>
   </Container>
 ));
