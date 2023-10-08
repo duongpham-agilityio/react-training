18dd87c3c8e7b0474d94a0ae7c71077ffe61a1f0
duong.pham <duong.pham@asnet.com.vn>
2023-10-08 15:00:18 +0700

    Add services endpoint constants

--- practices/practice-three/src/constants/url.ts
+++ practices/practice-three/src/constants/url.ts
@@ -14,10 +14,24 @@ export const ROUTES: Readonly<IRoutes> = {
   SignIn: 'sign-in',
 };
 
+//  Params
 export interface IParam {
   Product: string;
 }
 
 export const PARAM: Readonly<IParam> = {
   Product: ':product_id',
+};
+
+// Service Endpoints
+export interface IEndpointService {
+  Users: string;
+  Products: string;
+  Carts: string;
+}
+
+export const ENDPOINT_SERVICES: Readonly<IEndpointService> = {
+  Users: 'users',
+  Products: 'products',
+  Carts: 'carts',
 };
