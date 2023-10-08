a2c221a9cfd3e01cbe109ff4db09d0f4b8b3fa07
duong.pham <duong.pham@asnet.com.vn>
2023-10-08 23:47:27 +0700

    Render data for home page

--- practices/practice-three/src/pages/Home/index.tsx
+++ practices/practice-three/src/pages/Home/index.tsx
@@ -1,16 +1,54 @@
 import { memo, useCallback, useState } from 'react';
-import { Heading } from '@chakra-ui/react';
+import { Heading, Spinner, Square, Text } from '@chakra-ui/react';
+import { useQuery } from '@tanstack/react-query';
 
 // Components
 import { FilterBar } from '@/components';
 import { Products } from '@/pages/Home/components';
 
+// Hooks
+import { useProduct } from '@/hooks';
+import { ENDPOINT_SERVICES } from '@/constants';
+
+// Services
+import { productAPI } from '@/services/apis';
+import { MESSAGES } from '@/constants/message';
+
 const Component = (): JSX.Element => {
+  // Search
   const [filter, setFilter] = useState<string>('');
 
+  // Get products
+  const {
+    isLoading,
+    isError,
+    data = [],
+  } = useQuery({
+    queryKey: [ENDPOINT_SERVICES.Products],
+    queryFn: productAPI.getAll,
+  });
+
+  //  Handle with product
+  const { onAddFavorite, onAddToCart } = useProduct();
+
+  //  Handle change search value
   const onChangeFilter = useCallback((value: string): void => {
     setFilter(value);
   }, []);
+
+  if (isLoading)
+    return (
+      <Square size="full">
+        <Spinner />
+      </Square>
+    );
+
+  if (isError)
+    return (
+      <Square size="full">
+        <Text>{MESSAGES.FailToFetch}</Text>
+      </Square>
+    );
 
   return (
     <>
@@ -22,7 +60,7 @@ const Component = (): JSX.Element => {
       </Heading>
 
       {/* Render products */}
-      <Products />
+      <Products data={data} onLike={onAddFavorite} onAddToCart={onAddToCart} />
     </>
   );
 };
