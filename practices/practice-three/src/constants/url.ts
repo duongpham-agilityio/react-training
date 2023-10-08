adbcaec561034de04de97bc21eb0144c5138555c
duong.pham <duong.pham@asnet.com.vn>
2023-10-08 15:00:18 +0700

    Add services endpoint constants

--- practices/practice-three/src/constants/url.ts
+++ practices/practice-three/src/constants/url.ts
@@ -1,3 +1,4 @@
+// Browser Endpoints
 export interface IEndpoint {
   Root: string;
   Home: string;
@@ -14,10 +15,24 @@ export const ENDPOINT: Readonly<IEndpoint> = {
   SignIn: 'sign-in',
 };
 
+//  Params
 export interface IParam {
   Product: string;
 }
 
 export const PARAM: Readonly<IParam> = {
   Product: ':product_id',
+};
+
+// Service Endpoints
+export interface IEndpointService {
+  Users: string;
+  Products: string;
+  Carts: string;
+}
+
+export const ENDPOINT_SERVICES: Readonly<IEndpointService> = {
+  Users: 'users',
+  Products: 'products',
+  Carts: 'carts',
 };
