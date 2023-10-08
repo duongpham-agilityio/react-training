e91b144b7c128b6a064b5c7f972a8aa9a6758e17
duong.pham <duong.pham@asnet.com.vn>
2023-10-08 17:17:27 +0700

    Add comment for config axios

--- practices/practice-three/src/services/configs/axios.ts
+++ practices/practice-three/src/services/configs/axios.ts
@@ -1,5 +1,8 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
+export type IAxiosError = unknown & AxiosError;
+
+// Create new axios with baseURL
 export const apiRequest = axios.create({
   baseURL: import.meta.env.VITE_API,
 });
