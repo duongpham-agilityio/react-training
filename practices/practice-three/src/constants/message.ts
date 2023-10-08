e5abe238b6374a48a683e23eae013c7c22855871
duong.pham <duong.pham@asnet.com.vn>
2023-10-08 23:43:49 +0700

    Create constants

---
+++ practices/practice-three/src/constants/message.ts
@@ -0,0 +1,9 @@
+interface IMessages {
+  Empty: string;
+  FailToFetch: string;
+}
+
+export const MESSAGES: IMessages = {
+  Empty: 'No data',
+  FailToFetch: 'Something went wrong!! Please reload page.',
+};
