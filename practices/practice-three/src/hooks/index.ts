872188efca9856e21458c9b3910a18cee9c8e429
duong.pham <duong.pham@asnet.com.vn>
2023-10-10 01:29:12 +0700

    Create useFavorite hook

--- practices/practice-three/src/hooks/index.ts
+++ practices/practice-three/src/hooks/index.ts
@@ -2,3 +2,4 @@ export * from './useProduct';
 export * from './usePagination';
 export * from './useDebounce';
 export * from './useSearch';
+export * from './useFavorite';
---
+++ practices/practice-three/src/hooks/useFavorite.ts
@@ -0,0 +1,44 @@
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+
+// Styles
+import { IProduct } from '@/interface';
+
+export interface IUseFavorite {
+  data: IProduct[];
+  handleToggleFavorite: (product?: IProduct) => void;
+}
+
+export const useFavorite = create(
+  persist<IUseFavorite>(
+    (set, get) => ({
+      data: [],
+      handleToggleFavorite: (product?: IProduct): void => {
+        const { data } = get();
+
+        if (!product) {
+          return;
+        }
+
+        // check product exists in the store.
+        const isExist: boolean = !!data.find((item) => product.id === item.id);
+
+        // Remove from favorites list when Product already exists
+        if (isExist) {
+          const newFavorites = data.filter((item) => product.id !== item.id);
+
+          return set({ data: newFavorites });
+        }
+
+        // Add product to favorites
+        return set({
+          data: [...data, product],
+        });
+      },
+    }),
+    {
+      name: 'favorites',
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
+);
