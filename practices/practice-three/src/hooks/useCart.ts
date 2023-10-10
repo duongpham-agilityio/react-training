36e1e48addf7f57e8feb6e3ef243c804e54e7f88
duong.pham <duong.pham@asnet.com.vn>
2023-10-11 01:06:22 +0700

    Create useCart

--- practices/practice-three/src/hooks/useCart.ts
+++ practices/practice-three/src/hooks/useCart.ts
@@ -1,28 +1,34 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
+import { useCallback } from 'react';
+
+// Services
+import { productAPI } from '@/services/apis';
 
 // Types
 import { ICartData, IProduct } from '@/interface';
 
 export interface IUseCartStore {
   data: ICartData[];
-  handleAddToCart: (product: ICartData) => void;
+  addNewProduct: (product: ICartData) => void;
+  updateCart: (product: ICartData[]) => void;
 }
 
 export interface IUseCart {
-  data: ICartData[];
-  handleAddToCart: (product: IProduct) => boolean;
+  handleAddProductToCart: (product: IProduct) => boolean;
+  handleRemove: (productId: number) => void;
+  handleQuantity: (productId: number, quantity: number) => void;
+  handleCheckout: () => Promise<void>;
 }
 
-export type Selector<T> = (state: IUseCartStore) => T;
-
 //  Cart store
-const useCartStore = create(
+export const useCartStore = create(
   persist<IUseCartStore>(
     (set) => ({
       data: [],
-      handleAddToCart: (product: ICartData): void =>
+      addNewProduct: (product: ICartData): void =>
         set((state) => ({ data: [...state.data, product] })),
+      updateCart: (products: ICartData[]) => set({ data: products }),
     }),
     {
       name: 'cart',
@@ -30,59 +36,141 @@ const useCartStore = create(
   ),
 );
 
-export const useCart = () => {
-  const handleAddToCartStore = useCartStore(
-    (state: IUseCartStore) => state.handleAddToCart,
+export const useHandleCart = (): IUseCart => {
+  const addNewProduct = useCartStore(
+    (state: IUseCartStore): IUseCartStore['addNewProduct'] =>
+      state.addNewProduct,
   );
 
-  const cart: ICartData[] = useCartStore(
-    (state: IUseCartStore): ICartData[] => state.data,
+  const updateCart = useCartStore(
+    (state: IUseCartStore): IUseCartStore['updateCart'] => state.updateCart,
   );
 
-  console.log('Outside callback (cart)', cart);
+  const isValidQuantity = useCallback(
+    (quantityInCart: number, quantityInStock: number): boolean =>
+      quantityInCart <= quantityInStock && !!quantityInCart,
+    [],
+  );
 
-  const onAddToCart = (product: IProduct): boolean => {
-    const { id, description, imageURL, name, price, quantity } = product;
+  // Handle add product to cart
+  const handleAddProductToCart = useCallback(
+    (product: IProduct): boolean => {
+      try {
+        const { data: carts } = useCartStore.getState();
+        const { id, description, imageURL, name, price, quantity } = product;
 
-    console.log('Inside callback', cart);
-    console.log('Inside callback (get)', useCartStore.getState().data);
+        // Check Product exists from cart store
+        const isExist: ICartData | undefined = carts.find(
+          (item) => item.productId === id,
+        );
 
-    // Check Product exists from cart store
-    const isExist: ICartData | undefined = cart.find(
-      (item) => item.productId === id,
+        if (!isExist) {
+          const newRecord: ICartData = {
+            productId: id,
+            quantity: 1,
+            description,
+            imageURL,
+            name,
+            price,
+          };
+
+          addNewProduct(newRecord);
+
+          return true;
+        }
+
+        const isQuantityValid = isValidQuantity(isExist.quantity, quantity);
+
+        if (isQuantityValid) {
+          isExist.quantity += 1;
+          isExist.price = price * isExist.quantity;
+
+          updateCart([...carts]);
+
+          return true;
+        }
+
+        return false;
+      } catch (error) {
+        return false;
+      }
+    },
+    [addNewProduct, isValidQuantity, updateCart],
+  );
+
+  const handleRemove = useCallback(
+    (productId: number): void => {
+      const carts: ICartData[] = useCartStore.getState().data;
+      const newCarts: ICartData[] = carts.filter(
+        (cart: ICartData): boolean => cart.productId !== productId,
+      );
+
+      updateCart(newCarts);
+    },
+    [updateCart],
+  );
+
+  const handleQuantity = useCallback(
+    async (productId: number, quantity: number): Promise<void> => {
+      try {
+        // Get product from BE
+        const product: IProduct | undefined =
+          await productAPI.getById(productId);
+
+        if (!product) {
+          return;
+        }
+
+        const { price, quantity: quantityInStock } = product;
+        // Get carts from localStore
+        const carts: ICartData[] = useCartStore.getState().data;
+        // Get cart matching with productID
+        const cart: ICartData = carts.find(
+          (cart: ICartData) => cart.productId === productId,
+        ) as ICartData;
+
+        const isValid: boolean = isValidQuantity(quantity, quantityInStock);
+
+        if (!isValid) return;
+
+        // Update information
+        cart.price = price * quantity;
+        cart.quantity = quantity;
+
+        updateCart([...carts]);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [isValidQuantity, updateCart],
+  );
+
+  const handleCheckout = useCallback(async () => {
+    // Get products from DB
+    const products: IProduct[] = (await productAPI.getAll()) || [];
+    //  Get cart from localStore
+    const carts: ICartData[] = useCartStore.getState().data;
+
+    await Promise.all(
+      carts.map((cart: ICartData) => {
+        const { quantity, productId } = cart;
+        const product: IProduct | undefined = products.find(
+          (product: IProduct) => product.id === productId,
+        );
+
+        if (!product) return Promise.resolve();
+
+        return productAPI.update(productId, {
+          quantity: product.quantity - quantity,
+        });
+      }),
     );
-
-    if (!isExist) {
-      const newRecord: ICartData = {
-        productId: id,
-        quantity: 1,
-        description,
-        imageURL,
-        name,
-        price,
-      };
-
-      handleAddToCartStore(newRecord);
-
-      return true;
-    }
-
-    const isQuantityValid = isExist.quantity <= quantity;
-
-    if (isQuantityValid) {
-      isExist.quantity += 1;
-      isExist.price = price * isExist.quantity;
-
-      useCartStore.setState({ data: [...cart] });
-
-      return true;
-    }
-
-    return false;
-  };
+  }, []);
 
   return {
-    data: cart,
-    onAddToCart,
+    handleAddProductToCart,
+    handleRemove,
+    handleQuantity,
+    handleCheckout,
   };
 };
