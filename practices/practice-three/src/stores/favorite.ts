ef0232d06583f7d581cd21e021f8d92eb3aef1fd
duong.pham <duong.pham@asnet.com.vn>
2023-10-13 13:44:05 +0700

    Create favorite store

---
+++ practices/practice-three/src/stores/favorite.ts
@@ -0,0 +1,28 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+// Constants
+import { STORES } from '@/constants';
+
+// Styles
+import { IProduct } from '@/interface';
+
+export interface IFavoriteStore {
+  data: IProduct[];
+  setNewProduct: (product: IProduct) => void;
+  updateStore: (products: IProduct[]) => void;
+}
+
+export const favoriteStore = create(
+  persist<IFavoriteStore>(
+    (set) => ({
+      data: [],
+      setNewProduct: (product: IProduct): void =>
+        set((state) => ({ data: [...state.data, product] })),
+      updateStore: (products: IProduct[]): void => set({ data: products }),
+    }),
+    {
+      name: STORES.FAVORITE,
+    },
+  ),
+);
