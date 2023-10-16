a30e088e4ad056f4718978389e5b34f4dba92431
duong.pham <duong.pham@asnet.com.vn>
2023-10-16 09:20:38 +0700

    Update typo

--- practices/practice-three/src/pages/SignIn/index.tsx
+++ practices/practice-three/src/pages/SignIn/index.tsx
@@ -48,14 +48,14 @@ const Component = () => {
     [onChange],
   );
 
-  const handleVerifySuccessFull = useCallback(() => {
+  const handleVerifySuccessFul = useCallback(() => {
     setAuth();
     redirect(ROUTES.ROOT, { replace: true });
   }, [redirect, setAuth]);
 
   const handleLogin = useCallback(async () => {
-    onLogin(formData, handleVerifySuccessFull);
-  }, [formData, handleVerifySuccessFull, onLogin]);
+    onLogin(formData, handleVerifySuccessFul);
+  }, [formData, handleVerifySuccessFul, onLogin]);
 
   // !ISSUES: Because it is currently being re-render, I must use useMemo to remember
   const renderFormHeader = useMemo(
